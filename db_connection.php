7348fa7750fbb8e7961f2161aa6f9b4fc4472558
Your Name <you@example.com>
2026-05-01 15:07:08 +0800

    database-connection

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root"; 
 $password = ""; 
-$dbname = "php_practice"; 
+$dbname = "pr_db"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
